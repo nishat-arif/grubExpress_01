31def84db749cad7bab6a0c897446b794811269e
nishat-arif <nishatarif1912@gmail.com>
2025-07-07 16:24:01 -0400

    code refactor

---
+++ src/Body.js
@@ -0,0 +1,13 @@
+ const Body= () => {
+    return (
+    <div>
+        <div className="search"></div>
+        <div className="restaurantList">
+            <h1>Restaurant List</h1>
+            {/* Add restaurant cards here */}
+        </div>
+
+    </div>
+    )};
+
+    export default Body;
