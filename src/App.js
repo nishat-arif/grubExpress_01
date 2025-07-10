ccb582c95ae7307695d15cfc9dfdfa9f6e0f27d4
nishat-arif <nishatarif1912@gmail.com>
2025-07-10 17:59:52 -0400

    added about and contact component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { RouterProvider } from "react-router-dom";
+
 
 const App =()=>{
     return (
